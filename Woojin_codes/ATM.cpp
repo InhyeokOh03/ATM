2677d63a22122785970405744bffd78a092cfe04
wookk456 <112364566+wookk456@users.noreply.github.com>
2023-11-29 17:40:17 +0900

    Add files via upload

--- Woojin_codes/ATM.cpp
+++ Woojin_codes/ATM.cpp
@@ -4,13 +4,33 @@
 using namespace std;
 
 namespace Records{
-    ATM::ATM(string inBankName, string inSerialNum, bool isMul, bool isBi)
+    ATM::ATM(string inBankName, string inSerialNum, CASH inCash, bool isMul, bool isBi)
     : mBankName(inBankName)
     , mSerialNum(inSerialNum)
+    , mCurrCash(inCash)
     , isMulti(isMul)
     , isBilingual(isBi)
     {
     }
 
-    
+    Transaction& ATM::addCurrTransaction(int inId, const std::string& inCardID, int inType, int inAmount){
+        Transaction theTransaction;
+        theTransaction.setCardNum(inCardID);
+        theTransaction.setTransType(inType);
+        theTransaction.setAmountOfMoney(inAmount);
+        currTransaction.push_back(theTransaction);
+        return currTransaction[currTransaction.size()-1];
+    }
+
+    Transaction& ATM::addLifetimeTransaction(int inId, const std::string& inCardID, int inType, int inAmount){
+        Transaction theTransaction;
+        theTransaction.setCardNum(inCardID);
+        theTransaction.setTransType(inType);
+        theTransaction.setAmountOfMoney(inAmount);
+        lifetimeTransaction.push_back(theTransaction);
+        return lifetimeTransaction[lifetimeTransaction.size()-1];
+    }
+
+
+
 }
--- Woojin_codes/ATM.h
+++ Woojin_codes/ATM.h
@@ -1,19 +1,24 @@
 #include <string>
 #include <vector>
+#include "Cash.h"
+#include "Transaction.h"
 
 namespace Records{
     class ATM{
         public:
+            ATM(std::string inBankName, std::string inSerialNum, CASH inCash, bool isMulti, bool isBi);
             void printCurrTransaction() const;
             void printLifetimeTransaction() const;
             void makeTXTFile();
+            Transaction& addCurrTransaction(int inId, const std::string& inCardID, int inType, int inAmount);
+            Transaction& addLifetimeTransaction(int inId, const std::string& inCardID, int inType, int inAmount);
 
         protected:
             std::string mBankName;
             std::string mSerialNum;
-            //CASH mCurrCash;
-            //std::vector<Transaction> currTransaction;
-            //std::vector<Transaction> lifetimeTransaction;
+            CASH mCurrCash;
+            std::vector<Transaction> currTransaction;
+            std::vector<Transaction> lifetimeTransaction;
             bool isMulti;
             bool isBilingual;
             int CASHINPUTMAX;
